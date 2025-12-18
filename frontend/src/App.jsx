a4b4e552a534c6ab2c38c6d296830d6efdb8a408
md-tahmid <mdtahmid0003@gmail.com>
2025-12-18 13:17:43 +0600

    add best keyword

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,26 +7,26 @@ import Contact from './components/Contact'
 import Footer from './components/Footer'
 const App = () => {
   return (
-      <div className=''>
-        <div>
-          <Nav />
-        </div>
-        <section id='home'>
-          <Home />
-        </section>
-        <section id='about'>
-          <About />
-        </section>
-        <section id='projects'>
-          <Projects />
-        </section>
-        <section id='contact'>
-          <Contact />
-        </section>
-        <footer>
-          <Footer />
-        </footer>
+    <div className=''>
+      <div>
+        <Nav />
       </div>
+      <section id='home'>
+        <Home />
+      </section>
+      <section id='about'>
+        <About />
+      </section>
+      <section id='projects'>
+        <Projects />
+      </section>
+      <section id='contact'>
+        <Contact />
+      </section>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   )
 }
 
